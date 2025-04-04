01a807f1a21b63635b336d207f58e08bf2eff93b
Khushi Sinha <khushisinha4786@gmail.com>
2025-04-04 21:10:11 +0530

    first commit

---
+++ ollama-rag-chatbot-backend/src/main/java/com/example/ollama_rag_chatbot/OllamaRagChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ollama_rag_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaRagChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaRagChatbotApplication.class, args);
+	}
+
+}
---
+++ ollama-rag-chatbot-backend/src/test/java/com/example/ollama_rag_chatbot/OllamaRagChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ollama_rag_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaRagChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
